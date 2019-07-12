60f2f2a72ca19b02eeac6b59ea238625049849ff
suzzybliss <susanmajebi10.gmail.com>
2019-07-12 17:50:39 +0100

    User post property advert
    167247104

--- src/controllers/property.js
+++ src/controllers/property.js
@@ -1,1 +1,27 @@
+import models from '../models';
 
+const { Property } = models;
+
+export default {
+  create: (req, res) => {
+    const { body: { type, price, description }, user: { userId } } = req;
+
+    const property = {
+      id: Property.list.length + 1,
+      userId,
+      status,
+      price,
+      city,
+      address,
+      type,
+      created_on,
+      image_url,
+      description
+      
+    };
+
+    // persist property to database
+    Property.create(property);
+    return res.jsend.success(property);
+  },
+}
