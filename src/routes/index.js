4de105f41eaae0ca3723b8ae5b435adaa5ac7913
suzzybliss <susanmajebi10.gmail.com>
2019-07-12 18:52:10 +0100

    View all properties
    users can view all properties
    
    [finishes #167247180]

--- src/routes/index.js
+++ src/routes/index.js
@@ -14,4 +14,7 @@ router.post('/login', validator.auth, users.login);
 // Create property
 router.post('/property', authenticator, validator.property, property.create);
 
+// Get all property
+router.get('/property', authenticator, property.findAll);
+
 export default router;
