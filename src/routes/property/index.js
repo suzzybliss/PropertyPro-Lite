4585c988cb591e625c812a7c9f747c9c10b1b494
suzzybliss <susanmajebi10.gmail.com>
2019-07-16 20:36:55 +0100

    ch-securing API endpoints
    [finishes #167329196]
    :x

--- src/routes/property/index.js
+++ src/routes/property/index.js
@@ -6,8 +6,8 @@ import multer from '../../middlewares/multer';
 const router = express.Router();
 
 router.post('/', [auth, multer.single('image_url')], newProperty);
-router.get('/', getAllProperty);
-router.get('/:property_id', getProperty)
+router.get('/', auth, getAllProperty);
+router.get('/:property_id', auth, getProperty)
 router.patch('/:property_id/sold', auth, markPropertyAsSold);
 router.delete('/:property_id', auth, removeProprty);
 router.patch('/:property_id', auth, updateProperty);
