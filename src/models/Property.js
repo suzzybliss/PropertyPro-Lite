ceb821eab428955772b4ea46e6a734173a26ffee
Suzzy_Bliss <50342274+suzzybliss@users.noreply.github.com>
2019-07-24 09:42:49 +0100

    Delete Property.js

--- src/models/Property.js
+++
@@ -1,8 +0,0 @@
-const Property = {
-  create: property => Property.list.push(property),
-  findAll: userId => Property.list.filter(property => property.userId === userId),
-  findOne: propertyId => Property.list.find(property => property.id === propertyId),
-  list: []
-};
-
-export default Property;
